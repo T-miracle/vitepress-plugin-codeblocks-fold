9100af18845aeff4b85978dcebe1f581bb990d7a
T-miracle <1720570275@qq.com>
2023-04-16 14:33:19 +0800

    ...

--- lib/index.ts
+++ lib/index.ts
@@ -84,10 +84,10 @@ const judge = (el: HTMLElement, height: number) => {
  * @param height 限制高度
  */
 const fold = (el: HTMLElement, height: number) => {
-    if (el.querySelector('.fold-btn')) {
+    if (el.classList.contains('fold')) {
         return;
     }
-    console.log('生成折叠...');
+    el.classList.add('fold')
     const pre = el.querySelector('pre')!;
     pre.style.height = height + 'px';
     pre.style.overflow = 'hidden';
